1f7fcd0b86ff7a986abb1e519f82a6702c2132de
SenorGato <senoraraton@gmail.com>
2022-02-16 03:29:21 -0800

    Cleaned up stoneparse

--- Stone/stone.sh
+++ Stone/stone.sh
@@ -2,7 +2,8 @@
 
 IP=$(curl -s https://ipinfo.io/ip)
 LOC_QUERY=$(curl http://ip-api.com/csv/$IP)
-IFS=',' read -ra ADDR <<< "$LOC_QUERY"
+IFS=',' read -ra ADDR <<< "$LOC_QUERY
 LAT=${ADDR[7]}
 LONG=${ADDR[8]}
 curl -s 'https://api.sunrise-sunset.org/json?lat=$LAT&lng=$LONG&date=today' > ~/scripts/Stone/stone.data 
+
--- Stone/stonerefresh.sh
+++ Stone/stonerefresh.sh
@@ -1,4 +1,10 @@
-#Parses stone.data
+LOGVAL=date -r ~/scripts/Stone/stone.data
+
+if [$CURVAL==$LOGVAL]
+then
+    exit[0]
+fi
+
 SUNRISE=$(cat ~/scripts/Stone/stone.data | jq '.results .sunrise')
 SUNSET=$(cat ~/scripts/Stone/stone.data | jq '.results .sunset')
 SOLARNOON=$(cat ~/scripts/Stone/stone.data | jq '.results .solar_noon')
@@ -9,3 +15,5 @@ ATBEGIN=$(cat ~/scripts/Stone/stone.data | jq '.results .astronomical_twilight_b
 ATEND=$(cat ~/scripts/Stone/stone.data | jq '.results .astronomical_twilight_end')
 NTBEGIN=$(cat ~/scripts/Stone/stone.data | jq '.results .nautical_twilight_begin')
 NTEND=$(cat ~/scripts/Stone/stone.data | jq '.results .nautical_twilight_end')
+
+CURVAL=$LOGVAL
