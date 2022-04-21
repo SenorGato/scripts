cd34c0d8974d940a45e31fecfd27062fd5ea92f0
Claus Nomi <senoraraton@gmail.com>
2022-04-21 13:24:36 -0700

    Update configimport.sh

--- configimport.sh
+++ configimport.sh
@@ -1,6 +1,8 @@
 !#/bin/sh
 #YCM requirements
-apt install mono-complete golang nodejs default-jdk npm cmake python3-dev nvim build-essential
+apt-get update
+apt install mono-complete golang nodejs default-jdk npm cmake python3-dev nvim build-essential python3.9-dev
+
 #Repo clone
 alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
 cd ~
@@ -8,6 +10,12 @@ echo ".cfg" >> .gitignore
 git clone --bare https://github.com/SenorGato/dotfiles $HOME/.cfg
 alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
 config checkout
+config config --local status.showUntrackedFiles no
+
 #Plug init
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+nvim "+PlugInstall" "qa"
+
 #YCM install
+cd ~/.vim/plugged/youcompleteme
+python3 install.py --all
